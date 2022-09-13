e969d5e9a87c439dfd25636dfb150bc7ac57ada0
esparig <kinahchan@gmail.com>
2022-09-13 18:10:58 +0200

    Added checkpointing

--- dockerfiles/tf-cancernet/Scripts/pyimagesearch/cancernet.py
+++ dockerfiles/tf-cancernet/Scripts/pyimagesearch/cancernet.py
@@ -1,64 +1,57 @@
 # import the necessary packages
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.layers import SeparableConv2D
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Dense
-from tensorflow.keras import backend as K
+import tensorflow as tf
+
 class CancerNet:
 	@staticmethod
 	def build(width, height, depth, classes):
 		# initialize the model along with the input shape to be
 		# "channels last" and the channels dimension itself
-		model = Sequential()
+		model = tf.keras.models.Sequential()
 		inputShape = (height, width, depth)
 		chanDim = -1
 
 		# if we are using "channels first", update the input shape
 		# and channels dimension
-		if K.image_data_format() == "channels_first":
+		if tf.keras.backend.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 			chanDim = 1
         
         # CONV => RELU => POOL
-		model.add(SeparableConv2D(32, (3, 3), padding="same",
+		model.add(tf.keras.layers.SeparableConv2D(32, (3, 3), padding="same",
 			input_shape=inputShape))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+		model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+		model.add(tf.keras.layers.Dropout(0.25))
 		# (CONV => RELU => POOL) * 2
-		model.add(SeparableConv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(SeparableConv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+		model.add(tf.keras.layers.SeparableConv2D(64, (3, 3), padding="same"))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+		model.add(tf.keras.layers.SeparableConv2D(64, (3, 3), padding="same"))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+		model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+		model.add(tf.keras.layers.Dropout(0.25))
 		# (CONV => RELU => POOL) * 3
-		model.add(SeparableConv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(SeparableConv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(SeparableConv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+		model.add(tf.keras.layers.SeparableConv2D(128, (3, 3), padding="same"))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+		model.add(tf.keras.layers.SeparableConv2D(128, (3, 3), padding="same"))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+		model.add(tf.keras.layers.SeparableConv2D(128, (3, 3), padding="same"))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization(axis=chanDim))
+		model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+		model.add(tf.keras.layers.Dropout(0.25))
         # first (and only) set of FC => RELU layers
-		model.add(Flatten())
-		model.add(Dense(256))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization())
-		model.add(Dropout(0.5))
+		model.add(tf.keras.layers.Flatten())
+		model.add(tf.keras.layers.Dense(256))
+		model.add(tf.keras.layers.Activation("relu"))
+		model.add(tf.keras.layers.BatchNormalization())
+		model.add(tf.keras.layers.Dropout(0.5))
 		# softmax classifier
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+		model.add(tf.keras.layers.Dense(classes))
+		model.add(tf.keras.layers.Activation("softmax"))
 		# return the constructed network architecture
 		return model
--- dockerfiles/tf-cancernet/Scripts/training.py
+++ dockerfiles/tf-cancernet/Scripts/training.py
@@ -4,13 +4,38 @@ Python script to train the cancernet model.
 """
 
 import argparse
+import time
 import os
+from datetime import datetime
+from functools import wraps
 
 import tensorflow as tf
 
 from pyimagesearch.cancernet import CancerNet
 from pyimagesearch import config
 from pyimagesearch import paths
+
+
+def print_timing(func):
+    '''
+    create a timing decorator function
+    use
+    @print_timing
+    just above the function you want to time
+    '''
+    @wraps(func)  # improves debugging
+    def wrapper(*arg):
+        date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print(f'[{date_time}]')
+        start = time.perf_counter()  # needs python3.3 or higher
+        result = func(*arg)
+        end = time.perf_counter()
+        fs = '{} took {:.3f} seconds'
+        print(fs.format(func.__name__, (end - start)))
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        return result
+    return wrapper
 
 
 def parse_arguments():
@@ -20,12 +45,11 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser(description='Train the Cancernet model')
     #parser.add_argument('--model_summary', help='Shows model summary')
+    parser.add_argument('--ckpt', action='store_true', help='Enable checkpointing')
     parser.add_argument('--batch_size', default=32, type=int,
                         help='Batch size (default: 128)')
     parser.add_argument('--epochs', default=1, type=int,
                         help='number of training epochs (default: 10)')
-    # parser.add_argument('--ckpt_dir',
-    #                     help='Directory to save checkpoints')
     parser.add_argument('--learning_rate', default=1e-2, type=float,
                         help='Learning rate')
     parser.add_argument("-s", "--save", type=str, default="my_model.h5",
@@ -46,19 +70,33 @@ def parse_arguments():
     path_model = args.save
     print("Path to save the model: " + path_model)
 
-    # checkpoint_directory = None
-    # last_epoch = None
-    # if args.ckpt_dir:
-    #     if os.path.isdir(args.ckpt_dir):
-    #         checkpoint_directory = args.ckpt_dir
-    #         print("Checkpoint directory set to: " + checkpoint_directory)
-    #         last_epoch = get_last_epoch(checkpoint_directory)
-    #         if (last_epoch):
-    #             print("Last epoch: "+  str(last_epoch))
-    #     else:
-    #         print("Checkpoint directory doesn't exist! Continuing without checkpointing...")
+    checkpoint_directory = None 
+    last_epoch = None
+    if args.ckpt:
+        checkpoint_directory = config.CKPY_PATH
+        if os.path.isdir(checkpoint_directory):
+            print("Checkpoint directory set to: " + checkpoint_directory)
+            last_epoch = get_last_epoch(checkpoint_directory)
+            if (last_epoch):
+                print("Last epoch: "+  str(last_epoch))
+        else:
+            print("Checkpoint directory doesn't exist! Continuing without checkpointing...")
 
-    return batch_size, epochs, learning_rate, path_model
+    return batch_size, epochs, learning_rate, path_model, checkpoint_directory, last_epoch
+
+def get_last_epoch(checkpoint_directory):
+    """get_last_epoch
+    
+    Get last epoch that has been executed and checkpointed"""
+    epochs = []
+    if checkpoint_directory:
+        for i in os.listdir(checkpoint_directory):
+            try:
+                epochs.append(int(i[:-5].split('-')[1]))
+            except:
+                pass
+    return max(epochs) if (epochs) else None
+
 def get_train_labels():
     """get_train_labels
 
@@ -80,13 +118,31 @@ def get_class_weight(train_labels):
 
     return class_weight
 
+@print_timing
+def fit_cancernet(model, training_gen, batch_size, val_gen, callbacks, class_weight, epochs, last_epoch):
+    """fit_cancernet
+    
+    Train the model."""
+    
+    history = model.fit(
+        x=training_gen,
+        batch_size = batch_size,
+        validation_data=val_gen,
+        callbacks=callbacks,
+        class_weight=class_weight,
+        epochs=epochs,
+        initial_epoch=last_epoch)
+    
+    return history
+
+
 def run():
     """run
 
     Train model according to parsed arguments."""
 
     # construct the argument parser and parse the arguments
-    batch_size, epochs, learning_rate, path_model = parse_arguments()
+    batch_size, epochs, learning_rate, path_model, checkpoint_directory, last_epoch = parse_arguments()
 
     # Determine the total number of image paths in validation directories
     total_val = len(list(paths.list_images(config.VAL_PATH)))
@@ -145,28 +201,48 @@ def run():
 
     model.save(path_model)
 
-    callback = tf.keras.callbacks.EarlyStopping(
+    callbacks =  []
+    callbacks.append(tf.keras.callbacks.EarlyStopping(
         monitor='val_loss',
         min_delta=0,
         patience=3,
         verbose=0,
         mode='auto',
         baseline=None,
-        restore_best_weights=False
+        restore_best_weights=False)
     )
-    opt = tf.keras.optimizers.Adagrad(learning_rate=learning_rate, decay=learning_rate/epochs)
+    
+    opt = tf.keras.optimizers.Adagrad(learning_rate=learning_rate, decay=learning_rate/epochs) if epochs > 0 else "RMSprop"
 
     model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
+    if checkpoint_directory:
+        model_checkpoint_epoch_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_directory+"/ckpt-{epoch:03d}.hdf5",
+            save_weights_only=False,
+            monitor='val_accuracy',
+            mode='auto',
+            save_best_only=False,
+            save_freq="epoch")
+        model_checkpoint_best_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_directory+"/best.hdf5",
+            save_weights_only=False,
+            monitor='val_accuracy',
+            mode='auto',
+            save_best_only=True,
+            save_freq="epoch")
+        callbacks = [model_checkpoint_epoch_callback, model_checkpoint_best_callback]
+
+        if last_epoch:
+            model.load_weights(checkpoint_directory+f"/ckpt-{last_epoch:03d}.hdf5")
+        else:
+            last_epoch = 0
+    else:
+        last_epoch = 0
+        
     # fit the model
-    history = model.fit(
-        x=training_gen,
-        steps_per_epoch=total_train // batch_size,
-        validation_data=val_gen,
-        validation_steps=total_val // batch_size,
-        callbacks=[callback],
-        class_weight=class_weight,
-        epochs=epochs)
+    
+    history = fit_cancernet(model, training_gen, batch_size, val_gen, callbacks, class_weight, epochs, last_epoch)
 
     results = model.evaluate(x=test_gen)
     print("Evaluation of the model (test loss, test acc): ", results)
