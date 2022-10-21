d30e30d5da518a627d1897e3441951b4e2688a74
esparig <kinahchan@gmail.com>
2022-10-21 12:44:17 +0200

    added dockerfile and script for cluster dl test on denoising images using pytorch

---
+++ dockerfiles/pytorch-denoising/denoising.py
@@ -0,0 +1,153 @@
+from datetime import datetime
+from functools import wraps
+import time
+import torch
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+
+def print_timing(func):
+    '''
+    create a timing decorator function
+    use
+    @print_timing
+    just above the function you want to time
+    '''
+    @wraps(func)  # improves debugging
+    def wrapper(*arg):
+        date_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print(f'[{date_time}]')
+        start = time.perf_counter()  # needs python3.3 or higher
+        result = func(*arg)
+        end = time.perf_counter()
+        fs = '{} took {:.3f} seconds'
+        print(fs.format(func.__name__, (end - start)))
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        return result
+    return wrapper
+
+# convert data to torch.FloatTensor
+transform = transforms.ToTensor()
+
+# load the training and test datasets
+train_data = datasets.MNIST(root='data', train=True,
+                                   download=True, transform=transform)
+test_data = datasets.MNIST(root='data', train=False,
+                                  download=True, transform=transform)
+
+# Create training and test dataloaders
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+# define the NN architecture
+class ConvDenoiser(nn.Module):
+    def __init__(self):
+        super(ConvDenoiser, self).__init__()
+        ## encoder layers ##
+        # conv layer (depth from 1 --> 32), 3x3 kernels
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)  
+        # conv layer (depth from 32 --> 16), 3x3 kernels
+        self.conv2 = nn.Conv2d(32, 16, 3, padding=1)
+        # conv layer (depth from 16 --> 8), 3x3 kernels
+        self.conv3 = nn.Conv2d(16, 8, 3, padding=1)
+        # pooling layer to reduce x-y dims by two; kernel and stride of 2
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        ## decoder layers ##
+        # transpose layer, a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+        self.t_conv1 = nn.ConvTranspose2d(8, 8, 3, stride=2)  # kernel_size=3 to get to a 7x7 image output
+        # two more transpose layers with a kernel of 2
+        self.t_conv2 = nn.ConvTranspose2d(8, 16, 2, stride=2)
+        self.t_conv3 = nn.ConvTranspose2d(16, 32, 2, stride=2)
+        # one, final, normal conv layer to decrease the depth
+        self.conv_out = nn.Conv2d(32, 1, 3, padding=1)
+
+    def forward(self, x):
+        ## encode ##
+        # add hidden layers with relu activation function
+        # and maxpooling after
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        # add second hidden layer
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        # add third hidden layer
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)  # compressed representation
+        
+        ## decode ##
+        # add transpose conv layers, with relu activation function
+        x = F.relu(self.t_conv1(x))
+        x = F.relu(self.t_conv2(x))
+        x = F.relu(self.t_conv3(x))
+        # transpose again, output should have a sigmoid applied
+        x = F.sigmoid(self.conv_out(x))
+                
+        return x
+
+@print_timing
+def run():
+    # initialize the NN
+    model = ConvDenoiser()
+    print(model)
+
+    # specify loss function
+    criterion = nn.MSELoss()
+
+    # specify loss function
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # number of epochs to train the model
+    n_epochs = 20
+
+    # for adding noise to images
+    noise_factor=0.5
+
+    for epoch in range(1, n_epochs+1):
+        # monitor training loss
+        train_loss = 0.0
+        
+        ###################
+        # train the model #
+        ###################
+        for data in train_loader:
+            # _ stands in for labels, here
+            # no need to flatten images
+            images, _ = data
+            
+            ## add random noise to the input images
+            noisy_imgs = images + noise_factor * torch.randn(*images.shape)
+            # Clip the images to be between 0 and 1
+            noisy_imgs = np.clip(noisy_imgs, 0., 1.)
+                    
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            ## forward pass: compute predicted outputs by passing *noisy* images to the model
+            outputs = model(noisy_imgs)
+            # calculate the loss
+            # the "target" is still the original, not-noisy images
+            loss = criterion(outputs, images)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            # update running training loss
+            train_loss += loss.item()*images.size(0)
+                
+        # print avg training statistics 
+        train_loss = train_loss/len(train_loader)
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+            epoch, 
+            train_loss
+            ))
+        
+if __name__ == "__main__":
+    run()
